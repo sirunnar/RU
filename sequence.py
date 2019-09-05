fb9ae61cd8bf5fe9f0299b21c33bd2de063a4090
Unnar Sigurðsson <unnar@lyfogheilsa.is>
2019-09-05 18:53:39 +0000

    Að nálgast

--- sequence.py
+++ sequence.py
@@ -6,8 +6,23 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 counter = 0
 
+int_first = 1
+int_third = 0
+int_secont = 0
+int_forth = 0
+
+int_sum = 0
 
 while counter < n:
     print(counter, end="")
     counter += 1
-    
+
+    int_sum = int_forth + int_third + int_secont + int_first
+
+    int_forth = int_third
+    int_third = int_secont
+    int_secont = int_first
+    int_first = int_sum
+
+    print(int_sum)
+
